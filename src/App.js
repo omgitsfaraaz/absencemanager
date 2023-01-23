368083944984681fc855f9ea24b30a9cf737f34b
omgitsfaraaz <mohsinsait23@gmail.com>
2023-01-23 21:09:11 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -5,60 +5,7 @@ import { DatePicker, Table } from "antd";
 import Moment from "moment";
 
 const App = () => {
-  // const data = useMemo(
-  //   () => [
-  //     {
-  //       col1: "Hello",
-  //       col2: "World",
-  //     },
-  //     {
-  //       col1: "react-table",
-  //       col2: "rocks",
-  //     },
-  //     {
-  //       col1: "whatever",
-  //       col2: "you want",
-  //     },
-  //   ],
-  //   []
-  // );
-
-  // const columns = useMemo(
-  //   () => [
-  //     {
-  //       Header: "Column 1",
-  //       accessor: "col1", // accessor is the "key" in the data
-  //     },
-  //     {
-  //       Header: "Column 2",
-  //       accessor: "col2",
-  //     },
-  //   ],
-  //   []
-  // );
-
-  // const tableInstance = useTable({ columns, data });
-
-  // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-  //   tableInstance;
   const [combinedArr, setCombinedArr] = useState("");
-  const [slicedArr, setSlicedArr] = useState("");
-  const [sliceIndex, setSliceIndex] = useState(10);
-
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
 
   const columns = [
     {
@@ -87,8 +34,34 @@ const App = () => {
       dataIndex: "startDate",
       key: "startDate",
       render: (date) => {
-        console.log("sate", date);
-        return Moment(new Date(date)).format("MMM DD");
+        return Moment(new Date(date)).format("MMM DD YYYY");
+      },
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => (
+        <div>
+          <DatePicker
+            format={"MMM DD"}
+            allowClear={true}
+            onChange={(e) => {
+              setSelectedKeys([e]);
+              confirm();
+            }}
+          />
+        </div>
+      ),
+      onFilter: (value, record) => {
+        if (value) {
+          return (
+            Moment(new Date(record.startDate)).format("MMM DD") ==
+            Moment(new Date(value)).format("MMM DD")
+          );
+        } else {
+          return Moment(new Date(record.startDate)).format("MMM DD") !== null;
+        }
       },
     },
     {
@@ -114,7 +87,6 @@ const App = () => {
               confirm();
             }}
           />
-          <p onClick={() => confirm()}>hi</p>
         </div>
       ),
       onFilter: (value, record) => {
@@ -126,26 +98,45 @@ const App = () => {
         } else {
           return Moment(new Date(record.endDate)).format("MMM DD") !== null;
         }
-        // return (
-        //   Moment(new Date(record.endDate)).format("MMM DD") ==
-        //   Moment(new Date(value)).format("MMM DD")
-        // );
       },
     },
     {
       title: "Member note",
       dataIndex: "memberNote",
       key: "memberNote",
+      render: (value) => {
+        if (value) {
+          return value;
+        } else {
+          return "Note unavailable";
+        }
+      },
     },
     {
       title: "Status",
       dataIndex: "type",
       key: "type",
+      render: (value, record) => {
+        if (record.rejectedAt) {
+          return "Rejected";
+        } else if (record.confirmedAt) {
+          return "Confirmed";
+        } else {
+          return "Created";
+        }
+      },
     },
     {
       title: "Admitter note",
       dataIndex: "admitterNote",
       key: "admitterNote",
+      render: (value) => {
+        if (value) {
+          return value;
+        } else {
+          return "Note unavailable";
+        }
+      },
     },
   ];
 
@@ -156,15 +147,6 @@ const App = () => {
     dispatch(getAllMembers());
     dispatch(getAllAbsentees());
   }, []);
-
-  // useEffect(() => {
-  //   if (members && absentees) {
-  //     let res = absentees.payload.map((absentee) => {
-  //       return members.payload.filter((e) => e.userId == absentee.userId);
-  //     });
-  //     console.log("res in useEffect", res);
-  //   }
-  // }, [members, absentees]);
 
   useEffect(() => {
     if (members && absentees) {
@@ -181,71 +163,17 @@ const App = () => {
     }
   }, [members, absentees]);
 
-  useEffect(() => {
-    if (combinedArr) {
-      setSlicedArr(combinedArr.slice(0, sliceIndex));
-    }
-  }, [combinedArr]);
-
   const onChange = (pagination, filters, sorter, extra) => {
     console.log("params", pagination, filters, sorter, extra);
   };
   return (
     <div>
-      <h1>Absentees {combinedArr ? combinedArr.length : 0}</h1>
-      <div style={{ width: "90%", display: "flex", justifyContent: "center" }}>
-        <table border={1} width={"90%"} style={{ tableLayout: "fixed" }}>
-          <tr>
-            <th>Name</th>
-            <th>Type of absence</th>
-            <th>Period</th>
-            <th>Member note</th>
-            <th>Status</th>
-            <th>Admitter note</th>
-          </tr>
-          {slicedArr &&
-            slicedArr?.map((absentMember) => (
-              <tr>
-                <td style={{ textAlign: "center" }}>{absentMember.name}</td>
-                <td style={{ textAlign: "center" }}>{absentMember.type}</td>
-                <td style={{ textAlign: "center" }}>{absentMember.type}</td>
-                <td style={{ textAlign: "center" }}>
-                  {absentMember.memberNote}
-                </td>
-                <td style={{ textAlign: "center" }}>status</td>
-                <td style={{ textAlign: "center" }}>
-                  {absentMember.admitterNote}
-                </td>
-              </tr>
-            ))}
-          {/*
-            <td style={{ textAlign: "center" }}>hi</td>
-            <td>hi</td>
-            <td>hi</td>
-            <td>hi</td>
-            <td>hi</td>
-            <td>hi</td>
-              */}
-        </table>
-      </div>
+      <h1 style={{ textAlign: "center" }}>Absence Manager</h1>
 
-      <div>
-        <button
-          onClick={() => {
-            setSlicedArr(combinedArr.slice(sliceIndex, sliceIndex + 10));
-            setSliceIndex(sliceIndex + 10);
-          }}
-        >
-          {"<"}
-        </button>
-        <button
-          onClick={() => {
-            setSlicedArr(combinedArr.slice(sliceIndex - 10, sliceIndex));
-            setSliceIndex(sliceIndex - 10);
-          }}
-        >
-          {">"}
-        </button>
+      <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
+        <h3 style={{ marginTop: "20px", marginBottom: "20px" }}>
+          Total Absentees: {combinedArr ? combinedArr.length : 0}
+        </h3>
         <Table dataSource={combinedArr} columns={columns} onChange={onChange} />
       </div>
     </div>
